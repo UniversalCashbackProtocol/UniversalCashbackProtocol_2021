0412f7f4878e1a7fd3528f68ce021acda693bf37
Gilberts Ahumada <gilbertsahumada@gmail.com>
2021-11-05 14:04:03 -0300

    Update Store.sol
    se agregan funcionalidades para crear promociones, crear productos, asignar y desasignar productos a promociones

--- Store.sol
+++ Store.sol
@@ -1,18 +1,17 @@
 pragma solidity 0.8.0;
 
+
 contract Store{
-    
     uint256 private id;
     uint256 private qtyPromotions;
     uint256 private qtyProducts;
+    uint256 private counterProductsAsigned;
     string private nameStore;
     address private contractOwner;
     
-    enum PromotionSte {OPEN, CLOSED}
     
     struct Promotion{
         uint256 id;
-        PromotionSte state;
         uint256 startDate;
         uint256 endDate;
         bool active;
@@ -25,8 +24,15 @@ contract Store{
         uint256 tokenGivens;
     }
     
+    struct ProductAsigned{
+        uint256 idProduct;
+        bool asigned;
+    }
+    
     mapping(uint256 => Product) products;
     mapping(uint256 => Promotion) promotions;
+    //Mapping para cada producto asignado a una promocion
+    mapping(uint256 => mapping(uint256 => ProductAsigned)) productsInPromotions;
     
     constructor(address _owner, uint256 _id, string memory _nameStore){
         require(_owner != address(0) && _id > 0, "El address debe ser valido y el Id mayor a 0");
@@ -34,25 +40,53 @@ contract Store{
         qtyPromotions = 1;
         nameStore = _nameStore;
         contractOwner = _owner;
+        counterProductsAsigned = 0;
     }
     
-    function CreateProduct(string memory _SKU, uint256 _priceProduct, uint256 _tokensPerProduct) public{
+    function CreateProduct(string memory _SKU, uint256 _priceProduct, uint256 _tokensPerProduct) isOwner public{
+        require(_priceProduct >= 0 && _tokensPerProduct >= 0, "El Precio del producto y los Tokens a asignar deben ser mayor o igual a cero");
         products[qtyProducts] = Product(qtyProducts, _SKU, _priceProduct, _tokensPerProduct);
+        qtyProducts++;
     }
     
-    function CreatePromotion() public{
+    function CreatePromotion(uint256 _startDate, uint256 _endDate) isOwner public{
+        promotions[qtyPromotions] = Promotion(qtyPromotions, _startDate, _endDate, true);
+        qtyPromotions++;
+    }
+    
+    function AssignProductToPromotion(uint _idProduct, uint _idPromotion) isOwner public{
+        require(_idProduct > 0 && _idPromotion > 0, "Deben ser mayor a cero");
+        Promotion memory prom = GetPromotionById(_idPromotion);
+        Product memory prod = GetProductById(_idProduct);
+        counterProductsAsigned++;
+        productsInPromotions[counterProductsAsigned][prom.id] = ProductAsigned(prod.id, true);
         
     }
     
-
+    function UnAssignProductToPromotion(uint _idProduct, uint _idPromotion) isOwner public{
+        require(_idProduct > 0 && _idPromotion > 0, "Deben ser mayor a cero");
+        Promotion memory prom = GetPromotionById(_idPromotion);
+        Product memory prod = GetProductById(_idProduct);
+        productsInPromotions[counterProductsAsigned][prom.id] = ProductAsigned(prod.id, false);
+    }
     
-    function AssignProductToPromotion() public{
-        
+    function GetPromotionById(uint256 _id) public view returns (Promotion memory){
+        require(_id > 0, "El Id debe ser mayor a 0");
+        Promotion memory p = promotions[_id];
+        require (p.id > 0, "La promocion ingreasada no existe");
+        return p;
+    }
+    
+    function GetProductById(uint256 _id) public view returns (Product memory){
+        require(_id > 0, "El Id debe ser mayor a 0");
+        Product memory p = products[_id];
+        require(p.id > 0, "El producto no existe");
+        return p;
     }
     
     function BuyProduct() public{
         
-    }    
+    }  
     
     function owner() public view returns(address){
         return contractOwner;
@@ -63,3 +97,4 @@ contract Store{
         _;
     }
 }
+
